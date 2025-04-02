81edb5ab990be85a47276f8d96cdd2e70b182539
jborjanic <164763568+jborjanic@users.noreply.github.com>
2025-04-02 22:19:13 +0200

    Update FileInMemory.java

--- src/memory/FileInMemory.java
+++ src/memory/FileInMemory.java
@@ -4,43 +4,38 @@ import java.util.Arrays;
 
 public class FileInMemory {
     private String name;
-    private int size; // Velicina fajla u bajtovima
-    private int startBlock; // Početni blok u memoriji
-    private int length; // Broj zauzetih blokova
-    private byte[] contentFile; // Sadržaj fajla
+    private int size;
+    private int startBlock;
+    private int length;
+    private byte[] contentFile; 
 
     public FileInMemory(String name, byte[] content) {
         this.name = name;
         this.size = content.length;
         this.contentFile = Arrays.copyOf(content, content.length);
-        this.length = (int) Math.ceil((double) size / Block.getSize()); // Koliko blokova fajl zauzima
+        this.length = (int) Math.ceil((double) size / Block.getSize()); 
     }
-
-    // Vraća podatke u delovima (po blokovima)
+    
     public byte[] part(int index) {
         int blockSize = Block.getSize();
         byte[] part = new byte[blockSize];
         int startIndex = index * blockSize;
 
-        // Ako tražimo deo koji je van opsega, vraćamo prazan blok
         if (startIndex >= contentFile.length) {
-            Arrays.fill(part, (byte) ' '); // Popunjavanje praznim bajtovima
+            Arrays.fill(part, (byte) ' '); 
             return part;
         }
 
-        // Kopiramo podatke u blok
         int lengthToCopy = Math.min(blockSize, contentFile.length - startIndex);
         System.arraycopy(contentFile, startIndex, part, 0, lengthToCopy);
 
-        // Popunimo ostatak praznim bajtovima ako je potrebno
         if (lengthToCopy < blockSize) {
-            Arrays.fill(part, lengthToCopy, blockSize, (byte) ' '); // Popunjavanje ostatka bloka
+            Arrays.fill(part, lengthToCopy, blockSize, (byte) ' '); 
         }
 
         return part;
     }
 
-    // Getteri i setteri
     public int getStartBlock() {
         return startBlock;
     }
@@ -51,7 +46,7 @@ public class FileInMemory {
 
     public int getLength() {
         return length;
-    } // Vrati broj blokova
+    } 
 
     public void setLength(int length) {
         this.length = length;
@@ -59,7 +54,7 @@ public class FileInMemory {
 
     public int getSize() {
         return size;
-    } // Vrati broj bajtova
+    } 
 
     public String getName() {
         return name;
