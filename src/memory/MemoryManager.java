8a8adde426f91ded2400424011370203c77ef087
jborjanic <164763568+jborjanic@users.noreply.github.com>
2025-04-02 22:20:53 +0200

    Update MemoryManager.java

--- src/memory/MemoryManager.java
+++ src/memory/MemoryManager.java
@@ -7,7 +7,7 @@ import kernel.Process;
 
 public class MemoryManager {
 
-	public static ArrayList<PartitionMemory> partitionsInRam;   // zasto se uzima Ram? - primarna memorija; znamo sta je trenutno aktivno
+	public static ArrayList<PartitionMemory> partitionsInRam; 
 
 	public MemoryManager() {
 		Ram.initialize();
@@ -16,9 +16,9 @@ public class MemoryManager {
 	}
 
 	public int loadProcess(Process process) {  
-	    PartitionMemory partitionMemory = PartitionMemory.getPartitionByProcess(process); // Dobija memorijsku particiju za proces
+	    PartitionMemory partitionMemory = PartitionMemory.getPartitionByProcess(process);
 
-	    if (partitionMemory == null) { // Ako proces još nije u memoriji, alociraj mu particiju
+	    if (partitionMemory == null) { 
 	        partitionMemory = PartitionMemory.allocatePartition(process);
 	        if (partitionMemory == null) {
 	            throw new IllegalStateException("There are no free partitions for the process." + process.getPid());
@@ -28,20 +28,20 @@ public class MemoryManager {
 	    if (!partitionsInRam.contains(partitionMemory)) { 
 	        return loadPartition(partitionMemory);  
 	    } else {
-	        return process.getStartAdress(); // Ako je proces već u RAM-u, vrati njegovu početnu adresu
+	        return process.getStartAdress();
 	    }
 	}
 
-	public int loadPartition(PartitionMemory partition) { // za ucitavanje particije u RAM, ako postoji slobodna memorijska particija
-		PartitionMemory freePartition = findFreePartition(); // nadji slobodnu particiju
+	public int loadPartition(PartitionMemory partition) { 
+		PartitionMemory freePartition = findFreePartition(); 
 
 		if (freePartition != null) {
 			freePartition.setProcess(partition.getProcess());
-			freePartition.setData(partition.getData());         // postavlja sadrzaj memorije
+			freePartition.setData(partition.getData());        
 			partitionsInRam.add(freePartition);
-			return freePartition.getPositionInMemory();             // vraca pocetnu memorijsku adresu gde je proces ucitan    
+			return freePartition.getPositionInMemory();          
 		}
-		return -1; // Nema slobodnih particija
+		return -1; 
 	}
 
 	private PartitionMemory findFreePartition() {
@@ -54,14 +54,13 @@ public class MemoryManager {
 	}
 
 	public int[] readProcess(Process process) {
-		return readPartition(PartitionMemory.getPartitionByProcess(process));  // pronalazi memorijsku particiju gde je proces smjesten
-                                                                                       // cita podatke iz particije
+		return readPartition(PartitionMemory.getPartitionByProcess(process));  
+                                                                                    
 	}
 
 	public int[] readPartition(PartitionMemory partition) {
 		if (partitionsInRam.contains(partition))
 			return Ram.readPartition(partition);
-			//return Ram.readPartition(partition.getPositionInMemory(), partition.getSize());
 		return null;
 	}
 
@@ -72,7 +71,6 @@ public class MemoryManager {
 	public static boolean removePartition(PartitionMemory partition) {
 		if (partitionsInRam.contains(partition)) {
 			Ram.clearPartition(partition);
-		   //Ram.clearPartition(partition.getPositionInMemory(), partition.getSize());
 			partition.setProcess(null); 
 			partitionsInRam.remove(partition);
 			return true;
@@ -80,11 +78,11 @@ public class MemoryManager {
 		return false;
 	}
 
-	public static int memoryOccupiedByProcess(Process process) {  // vraca velicinu memorije zauzetu od strane tog procesa
+	public static int memoryOccupiedByProcess(Process process) {  
 		for (PartitionMemory partition : partitionsInRam)
 			if (partition.getProcess().getPid() == process.getPid())
 				return partition.getSize();    
-		return 0;   // ako proces nije pronadjen, vraca 0
+		return 0;   
 	}
 	
 	public static void printMemory() {
