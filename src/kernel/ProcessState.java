1a67b2016979eedf7cc9bdfb9630142905c414b8
jborjanic <jovana.borjanic000@gmail.com>
2025-05-10 14:46:07 +0200

    Rjesavanje konflikta.

--- src/kernel/ProcessState.java
+++ src/kernel/ProcessState.java
@@ -1,27 +1,43 @@
 package kernel;
 
+
+
 public enum ProcessState
+
 {
+
 	READY, RUNNING, BLOCKED, TERMINATED, DONE;
+
 	
+
 	public static String state(ProcessState state) {
+
 		if (state == READY)
+
 			return "READY";
+
 		else if (state == RUNNING)
+
 			return "RUNNING";
+
 		else if (state == BLOCKED)
+
 			return "BLOCKED";
+
 		else if (state == TERMINATED)
+
 			return "TERMINATED";
+
 		else if (state == DONE)
+
 			return "DONE";
+
 		return "";
+
 	}
-<<<<<<< HEAD
-=======
+
 	
 
->>>>>>> c1348b79ca788e3df565672edfd4cebf722d1ee1
+
+
 }
-
-
