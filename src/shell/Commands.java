df49105edaa45ab7fa77522a2d5cf289302a57af
jborjanic <jovana.borjanic000@gmail.com>
2025-05-10 14:45:15 +0200

    Rjesavanje konflikta.

--- src/shell/Commands.java
+++ src/shell/Commands.java
@@ -8,27 +8,27 @@ import java.util.ArrayList;
 
 public class Commands {
     private static String command;  
-    private static ArrayList<String> commandList = new ArrayList<>(); 
+    private static ArrayList<String> commandList = new ArrayList<>();
     private static int iter;  //broj unijetih komandi do sada
     private static PrintStream outStream;
 
     public static void returnCommand() {
-        commandList.add(command); 
-        iter = commandList.size(); 
+        commandList.add(command);
+        iter = commandList.size();
         command = command.toLowerCase();  
-        String[] commands = command.split(" "); 
+        String[] commands = command.split(" ");
 
         switch (commands[0]) {
 
             case "ls":
                 if (commands.length == 1) {
-                    Exe.ls(); 
+                    Exe.ls();
                 } else errorWithParameters();
                 break;
 
             case "cd":
                 if (commands.length == 2) {
-                    Exe.cd(commands[1]); 
+                    Exe.cd(commands[1]);
                 } else errorWithParameters();
                 break;
 
@@ -40,7 +40,7 @@ public class Commands {
 
             case "load":
                 if (commands.length == 2) {
-                    Exe.load(commands[1]); 
+                    Exe.load(commands[1]);
                 } else errorWithParameters();
                 break;
 
@@ -50,13 +50,13 @@ public class Commands {
 
             case "pr":
                 if (commands.length == 1) {
-                    Exe.pr(); 
+                    Exe.pr();
                 } else errorWithParameters();
                 break;
 
             case "trm":
                 if (commands.length == 2) {
-                    Exe.trm(commands[1]); 
+                    Exe.trm(commands[1]);
                 } else errorWithParameters();
                 break;
 
@@ -70,109 +70,53 @@ public class Commands {
                 System.out.println("That command doesn't exist!");
         }
     }
-    
-<<<<<<< HEAD
-    public static void setOut(OutputStream out) {
-        outStream = new PrintStream(out, true);
-        System.setOut(outStream); // Preusmerava System.out
-    }
-=======
+   
     public static String previous() {
-		String rez = "";
-		if (!commandList.isEmpty()) {
-			if (iter >= 0) {
-				iter--;
-				if (iter <= 0)
-					iter = 0;
-				rez = commandList.get(iter);
-			}
-		}
-		return rez;
-	}
->>>>>>> c1348b79ca788e3df565672edfd4cebf722d1ee1
-
-	public static String next() {
-		String rez = "";
-		if (!commandList.isEmpty())
-			if (iter < commandList.size() - 1) {
-				iter++;
-				if (iter > commandList.size() - 1)
-					iter = commandList.size() - 1;
-				rez = commandList.get(iter);
-			}
-		return rez;
-	}
-
-	public static void readACommand(PipedInputStream inp, int len) {
-		command = "";
-		char c;
-
-		for (int i = 0; i < len; i++) {
-			try {
-				c = (char) inp.read();
-				command += c;
-			} catch (IOException e) {
-				e.printStackTrace();
-				System.out.println("Error while reading a command");
-			}
-		}
-	}
-
-	public static void setOut(OutputStream out) {
-		System.setOut(new PrintStream(out, true));
-	}
-
-	private static void errorWithParameters() {
-		String s = "Parameters for command are incorrect!\n";
-		System.out.println(s);
-	}
+String rez = "";
+if (!commandList.isEmpty()) {
+if (iter >= 0) {
+iter--;
+if (iter <= 0)
+iter = 0;
+rez = commandList.get(iter);
+}
+}
+return rez;
 }
 
-<<<<<<< HEAD
-    private static void errorWithParameters() {
-        System.out.println("Parameters for command are incorrect!\n");
-    }
-    
-    
-    public static String previous() {
-		String rez = "";
-		if (!commandList.isEmpty()) {
-			if (iter >= 0) {
-				iter--;
-				if (iter <= 0)
-					iter = 0;
-				rez = commandList.get(iter);
-			}
-		}
-		return rez;
-	}
-
-	public static String next() {
-		String rez = "";
-		if (!commandList.isEmpty())
-			if (iter < commandList.size() - 1) {
-				iter++;
-				if (iter > commandList.size() - 1)
-					iter = commandList.size() - 1;
-				rez = commandList.get(iter);
-			}
-		return rez;
-	}
-
-	public static void readACommand(PipedInputStream inp, int len) {
-		command = "";
-		char c;
-
-		for (int i = 0; i < len; i++) {
-			try {
-				c = (char) inp.read();
-				command += c;
-			} catch (IOException e) {
-				e.printStackTrace();
-				System.out.println("Error while reading a command");
-			}
-		}
-	}
+public static String next() {
+String rez = "";
+if (!commandList.isEmpty())
+if (iter < commandList.size() - 1) {
+iter++;
+if (iter > commandList.size() - 1)
+iter = commandList.size() - 1;
+rez = commandList.get(iter);
 }
-=======
->>>>>>> c1348b79ca788e3df565672edfd4cebf722d1ee1
+return rez;
+}
+
+public static void readACommand(PipedInputStream inp, int len) {
+command = "";
+char c;
+
+for (int i = 0; i < len; i++) {
+try {
+c = (char) inp.read();
+command += c;
+} catch (IOException e) {
+e.printStackTrace();
+System.out.println("Error while reading a command");
+}
+}
+}
+
+public static void setOut(OutputStream out) {
+System.setOut(new PrintStream(out, true));
+}
+
+private static void errorWithParameters() {
+String s = "Parameters for command are incorrect!\n";
+System.out.println(s);
+}
+}
