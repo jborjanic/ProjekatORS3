dd0e9a700620e5063da3369ed2f49fad4beaac71
andjelabb <andjela.brdjanin@gmail.com>
2025-06-20 14:19:13 +0200

    Azurirana klasa Constants

--- src/asembler/Constants.java
+++ src/asembler/Constants.java
@@ -1,9 +1,9 @@
-package asembler;
+/*package asembler;
 
 public class Constants 
 {
-	public static final String R1="0000";
-	public static final String R2="0001";
-	public static final String R3="0010";
-	public static final String R4="0011";
-}
+	public static final String R0="0000";
+	public static final String R1="0001";
+	public static final String R2="0010";
+	public static final String R3="0011";
+}*/
